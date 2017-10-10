02f70ee831a058bab4fba8ab0a6a502b3173f690
calderarchinuk <calderarchinuk@gmail.com>
2017-10-10 11:12:25 -0700

    removed debug pause from main

--- ClientProject/ClientProject/ClientProject.cpp
+++ ClientProject/ClientProject/ClientProject.cpp
@@ -826,9 +826,8 @@ int main(int argc, char **argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 
-	RUN_ALL_TESTS();
-	system("pause");
-	return 0;
+	
+	return RUN_ALL_TESTS();
 
 	//should use only this in a build
 	//return RUN_ALL_TESTS();
